e0e6ce6391dfc30f321a60acfc9928105157d3b0
aaron saunders <aaronksaunders@gmail.com>
2023-03-07 16:45:31 -0500

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'vite.vue.cap',
+  appName: 'vite-vue-cap',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
