bf31d690c41d4660ed6678eebbc758aecff784a7
aaron saunders <aaronksaunders@gmail.com>
2023-03-10 00:04:10 -0500

    camera plist tested

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,29 @@
 <script setup lang="ts">
 import HelloWorld from "./components/HelloWorld.vue";
 import { ActionSheet, ActionSheetButtonStyle } from "@capacitor/action-sheet";
+import { Camera, CameraResultType } from '@capacitor/camera';
+import { ref } from "vue";
+
+// ref for image from camera
+const imageUrl = ref<string|undefined>()
+
+// camera
+const takePicture = async () => {
+  const image = await Camera.getPhoto({
+    quality: 90,
+    allowEditing: true,
+    resultType: CameraResultType.Uri
+  });
+
+  // image.webPath will contain a path that can be set as an image src.
+  // You can access the original file using image.path, which can be
+  // passed to the Filesystem API to read the raw data of the image,
+  // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
+
+  imageUrl.value = image.webPath;
+};
+
+// action sheet
 const showActionSheet = async () => {
   const result = await ActionSheet.showActions({
     title: "Photo Options",
@@ -33,7 +56,9 @@ const showActionSheet = async () => {
     </a>
   </div>
   <HelloWorld msg="Vite + Vue + Ionic Capacitor" />
-  <button @click="showActionSheet">SHOW ACTION SHEET</button>
+  <button @click="showActionSheet" class="action-button">SHOW ACTION SHEET</button>
+  <button @click="takePicture"  class="action-button">TAKE PICTURE</button>
+  <img :src="imageUrl" style="width: 100%"/>
 </template>
 
 <style scoped>
